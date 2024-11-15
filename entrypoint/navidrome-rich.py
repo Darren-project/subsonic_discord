7bccf9672e639bee0520299000cc68fc4526a135
mickeydarrenlau <darrenwjlau@gmail.com>
2024-11-15 19:21:15 +0800

    ddd

--- entrypoint/navidrome-rich.py
+++ entrypoint/navidrome-rich.py
@@ -101,7 +101,7 @@ while True:  # The presence will stay on as long as the program is running
           pass
        else:
          RPC.update(
-            large_image="plex_icon",
+            large_image="subsonic_icon",
             state="No music is playing",
             details="Play some music!",
             buttons=[{"label": "History", "url": shared.dashboard_url}],
